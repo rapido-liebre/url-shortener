d6129845dc7f7a7e0cf3061ccd55ef74578010b4
Rafał Zając <rafal.zajac@ext.saint-gobain.com>
2025-06-30 21:20:30 +0200

    Update frontend on Railway

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/links': 'http://localhost:8080',
-      '/u': 'http://localhost:8080'
-    }
-  }
+  base: '/',
 })
